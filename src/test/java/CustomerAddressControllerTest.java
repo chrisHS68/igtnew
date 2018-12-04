bab22e583cbe543dff0a3ef1bf8d3ea3b98f08c7
chris <1414414@stud.hs-mannheim.de>
2018-12-05 00:18:51 +0100

    Added tests

---
+++ src/test/java/CustomerAddressControllerTest.java
@@ -0,0 +1,25 @@
+import de.hsma.jens.controllers.CustomerAdressController;
+import de.hsma.jens.models.CustomerAddress;
+import de.hsma.jens.models.FlightCustomer;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerAddressControllerTest {
+    @Test
+    public void createAddressTest() {
+        CustomerAddress ca = new CustomerAddress();
+        ca.setStrasse("Test-Straße");
+        ca.setPlz("68305");
+        ca.setOrt("Mannheim");
+        ca.setHausnummer("4b");
+        CustomerAdressController customerAdressController = new CustomerAdressController();
+        List<CustomerAddress> addresses = new ArrayList<>();
+        addresses.add(ca);
+        customerAdressController.createAdress(addresses);
+        customerAdressController
+                .getAllAdress()
+                .forEach(customerAddress -> System.out.println(customerAddress.toString()));
+    }
+}
---
+++ src/test/java/FlightCustomerControllerTest.java
@@ -0,0 +1,48 @@
+import de.hsma.jens.controllers.FlightCustomerController;
+import de.hsma.jens.models.FlightCustomer;
+import de.hsma.jens.models.Status;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FlightCustomerControllerTest {
+    /**
+     * @Before public void setUp() throws Exception {
+     * <p>
+     * FlightCustomerController flightCustomerController = new FlightCustomerController();
+     * <p>
+     * //flightCustomerController.deleteAllCustomer();
+     * //flightCustomerController.createCustomers();
+     * <p>
+     * <p>
+     * }
+     **/
+    @Test
+    public void createCustomersTest() {
+
+        FlightCustomer fc = new FlightCustomer();
+        fc.setJahresmeilen(0);
+        fc.setGesamtmeilen(0);
+        fc.setCustomerStatus(Status.NONE);
+
+        FlightCustomerController flightCustomerController = new FlightCustomerController();
+        List<FlightCustomer> customers = new ArrayList<>();
+        customers.add(fc);
+        flightCustomerController.createFlightCustomers(customers);
+        flightCustomerController.getAllFlightCustomers().forEach(flightCustomer -> System.out.println(flightCustomer.toString()));
+
+    }
+/**
+ @After public void tearDown() throws Exception {
+
+ FlightCustomerController flightCustomerController = new FlightCustomerController();
+
+ //custController.deleteAllCustomer();
+
+
+ }
+ **/
+}
