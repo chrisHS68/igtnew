80ca273ff87f4cedfb63da2e015f7c18ec6032db
sbtarit <1514520@stud.hs-mannheim.de>
2018-12-04 13:44:49 +0100

    Updated controller

--- src/main/java/de/hsma/jens/controllers/FlightCustomerController.java
+++ src/main/java/de/hsma/jens/controllers/FlightCustomerController.java
@@ -1,14 +1,29 @@
 package de.hsma.jens.controllers;
 
-import de.hsma.jens.models.Airport;
-import de.hsma.jens.models.FlightCustomer;
+import de.hsma.jens.models.*;
+import de.hsma.jens.models.Status;
 import de.hsma.jens.tools.Config;
 import org.apache.log4j.Logger;
+import org.hibernate.CacheMode;
+import org.hibernate.ScrollMode;
+import org.hibernate.ScrollableResults;
+import org.hibernate.Session;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.transaction.TransactionManager;
+import javax.persistence.Query;
+import javax.transaction.*;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+
+import static java.nio.file.StandardOpenOption.APPEND;
+import static java.nio.file.StandardOpenOption.CREATE;
 
 public class FlightCustomerController {
     private static Logger logger = Logger.getRootLogger();
@@ -19,29 +34,433 @@ public class FlightCustomerController {
     EntityManagerFactory emf = Persistence.createEntityManagerFactory(Config.PERSISTENCE_UNIT_NAME);
 
     //Inteface Methods
-    public void createFlightCustomer(){
 
-    };
+    public void updateFlightCustomer(FlightCustomer fc) {
 
-    public int getFlightCustomer(int id){
-        return 1;
-    };
+        try {
+            logger.info("Update flightcustomer TA begins");
+            EntityManager em = emf.createEntityManager();
+            tm.setTransactionTimeout(Config.TRANSACTION_TIMEOUT);
+            tm.begin();
 
-    public List<FlightCustomer> getAllFlightCustomers(){
-        return null;
-    };
 
-    public void updateFlightCustomer(FlightCustomer fc){
+            long queryStart = System.currentTimeMillis();
 
-    };
-    public void FlightCustomer(List<FlightCustomer> fc){
+            FlightCustomer customerToUpdate = em.find(FlightCustomer.class, fc.getId());
+            logger.info("Found flightcustomer: " + customerToUpdate.toString());
+            logger.info("Updating flightcustomer...");
+            customerToUpdate = fc;
 
-    };
+            em.merge(customerToUpdate);
 
-    public void deleteFlightCustomer(int id){
+            long queryEnd = System.currentTimeMillis();
 
-    };
-    public void deleteAllFlightCustomer(){
 
-    };
+            em.flush();
+            em.close();
+            tm.commit();
+            logger.info("Update flightcustomer TA ends");
+
+            long queryTime = queryEnd - queryStart;
+
+            logger.info("FlightCustomer successfully persisted in " + queryTime + " ms.");
+
+        } catch (NotSupportedException e) {
+            e.printStackTrace();
+        } catch (SystemException e) {
+            e.printStackTrace();
+        } catch (HeuristicMixedException e) {
+            e.printStackTrace();
+        } catch (HeuristicRollbackException e) {
+            e.printStackTrace();
+        } catch (RollbackException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void updateFlightCustomer(List<FlightCustomer> flightCustomerList) {
+
+        try {
+            EntityManager em = emf.createEntityManager();
+            logger.info("Update flightcustomers TA begins");
+            tm.setTransactionTimeout(Config.TRANSACTION_TIMEOUT);
+            tm.begin();
+
+            long queryStart = System.currentTimeMillis();
+
+            for (FlightCustomer fc : flightCustomerList) {
+
+                em.merge(fc);
+
+            }
+
+            long queryEnd = System.currentTimeMillis();
+
+
+            em.flush();
+            em.close();
+            tm.commit();
+            logger.info("Update flightcustomers TA ends");
+
+            long queryTime = queryEnd - queryStart;
+
+            logger.info("Updates of " + flightCustomerList.size() + " flightcustomers successfully persisted in " + queryTime + " ms.");
+
+            String writeToFile = new String(Config.PERSISTENCE_UNIT_NAME + " UPDATE: " + flightCustomerList.size() + " " + queryTime + "\n");
+            Files.write(Paths.get(Config.LOG_STORAGE_LOCATION), writeToFile.getBytes(), CREATE, APPEND);
+
+
+        } catch (NotSupportedException e) {
+            e.printStackTrace();
+        } catch (SystemException e) {
+            e.printStackTrace();
+        } catch (HeuristicMixedException e) {
+            e.printStackTrace();
+        } catch (HeuristicRollbackException e) {
+            e.printStackTrace();
+        } catch (RollbackException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public void deleteFlightCustomer(int FlightCustomerID) {
+
+        try {
+            //logger.info("Delete flightcustomer TA begins");
+            EntityManager em = emf.createEntityManager();
+            tm.setTransactionTimeout(Config.TRANSACTION_TIMEOUT);
+            tm.begin();
+
+
+            // long queryStart = System.currentTimeMillis();
+
+            FlightCustomer cust = em.find(FlightCustomer.class, FlightCustomerID);
+            // logger.info("Found flightcustomer: " + cust.toString());
+            // logger.info("Deleting flightcustomer...");
+
+
+            em.remove(cust);
+
+            //  long queryEnd = System.currentTimeMillis();
+
+
+            em.flush();
+            em.close();
+            tm.commit();
+            //logger.info("Delete flightcustomer TA ends");
+
+            //  long queryTime = queryEnd - queryStart;
+
+            // logger.info("FlightCustomer successfully deleted in " + queryTime + " ms.");
+
+
+        } catch (NotSupportedException e) {
+            e.printStackTrace();
+        } catch (SystemException e) {
+            e.printStackTrace();
+        } catch (HeuristicMixedException e) {
+            e.printStackTrace();
+        } catch (HeuristicRollbackException e) {
+            e.printStackTrace();
+        } catch (RollbackException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public List<FlightCustomer> getALLFlightCustomer() {
+        List<FlightCustomer> cust = new ArrayList<FlightCustomer>();
+
+
+        try {
+            EntityManager em = emf.createEntityManager();
+
+            String queryString = new String("SELECT c FROM FlightCustomer c");
+
+            logger.info("Get all flightcustomer TA begins");
+            tm.setTransactionTimeout(Config.TRANSACTION_TIMEOUT);
+            tm.begin();
+
+
+            Query q = em.createQuery(queryString);
+
+            long queryStart = System.currentTimeMillis();
+
+            cust = q.getResultList();
+
+            long queryEnd = System.currentTimeMillis();
+
+
+            em.flush();
+            em.close();
+            tm.commit();
+
+            logger.info("Get all flightcustomer TA ends");
+
+            long queryTime = queryEnd - queryStart;
+
+            logger.info("Found " + cust.size() + " flightcustomers in " + queryTime + " ms.");
+
+            String writeToFile = new String(Config.PERSISTENCE_UNIT_NAME + " READ  : " + cust.size() + " " + queryTime + "\n");
+            Files.write(Paths.get(Config.LOG_STORAGE_LOCATION), writeToFile.getBytes(), CREATE, APPEND);
+
+
+        } catch (NotSupportedException e) {
+            e.printStackTrace();
+        } catch (SystemException e) {
+            e.printStackTrace();
+        } catch (HeuristicMixedException e) {
+            e.printStackTrace();
+        } catch (HeuristicRollbackException e) {
+            e.printStackTrace();
+        } catch (RollbackException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return cust;
+
+    }
+
+    public void deleteAllFlightCustomer() {
+
+        List<Integer> cust;
+
+        try {
+
+
+            cust = getAllFlightCustomerIDs();
+
+
+            logger.info("Delete all customers TA begins");
+
+
+            long queryStart = System.currentTimeMillis();
+
+            for (Integer id : cust) {
+
+                deleteFlightCustomer(id);
+            }
+
+
+            long queryEnd = System.currentTimeMillis();
+
+
+            logger.info("Delete all flightcustomers TA ends");
+            long queryTime = queryEnd - queryStart;
+
+            logger.info(cust.size() + " flightcustomers successfully deleted in " + queryTime + " ms.");
+
+            String writeToFile = new String(Config.PERSISTENCE_UNIT_NAME + " DELETE: " + cust.size() + " " + queryTime + "\n");
+            Files.write(Paths.get(Config.LOG_STORAGE_LOCATION), writeToFile.getBytes(), CREATE, APPEND);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public List<Integer> getAllFlightCustomerIDs() {
+
+        List<FlightCustomer> fcIDs = new ArrayList<FlightCustomer>();
+        List<Integer> returnList = new ArrayList<Integer>();
+
+        try {
+            EntityManager em = emf.createEntityManager();
+
+            //String queryString = new String("SELECT c.C_ID FROM Customer c");
+            /*
+            some datastores (that uses indexes like Redis, Infinispan, Cassandra) cannot operate on single return values,
+            they always need to return the entire class
+             */
+
+            String queryString = new String("SELECT c FROM Customer c");
+            Query q = em.createQuery(queryString);
+
+
+            logger.info("Get all flightcustomerIDs TA begins");
+            tm.setTransactionTimeout(Config.TRANSACTION_TIMEOUT);
+            tm.begin();
+
+            long queryStart = System.currentTimeMillis();
+
+            fcIDs = q.getResultList();
+
+            for (FlightCustomer fc : fcIDs) {
+                returnList.add(fc.getId());
+            }
+
+
+            long queryEnd = System.currentTimeMillis();
+
+
+            em.flush();
+            em.close();
+            tm.commit();
+            logger.info("Get all flightcustomerIDs TA ends");
+
+            long queryTime = queryEnd - queryStart;
+
+            logger.info("Found " + fcIDs.size() + " flightcustomer IDs in " + queryTime + " ms.");
+
+
+        } catch (NotSupportedException e) {
+            e.printStackTrace();
+        } catch (SystemException e) {
+            e.printStackTrace();
+        } catch (HeuristicMixedException e) {
+            e.printStackTrace();
+        } catch (HeuristicRollbackException e) {
+            e.printStackTrace();
+        } catch (RollbackException e) {
+            e.printStackTrace();
+        }
+
+
+        return returnList;
+
+    }
+
+    public FlightCustomer getFlightCustomer(int flightcustomerID) {
+
+
+        FlightCustomer cust = null;
+
+
+        try {
+            EntityManager em = emf.createEntityManager();
+            tm.begin();
+
+
+            cust = em.find(FlightCustomer.class, flightcustomerID);
+            // logger.info("Found customer: " + flightcust.toString());
+
+
+            em.flush();
+            em.close();
+            tm.commit();
+            // logger.info("TA ends");
+
+
+        } catch (NotSupportedException e) {
+            e.printStackTrace();
+        } catch (SystemException e) {
+            e.printStackTrace();
+        } catch (HeuristicMixedException e) {
+            e.printStackTrace();
+        } catch (HeuristicRollbackException e) {
+            e.printStackTrace();
+        } catch (RollbackException e) {
+            e.printStackTrace();
+        }
+
+
+        return cust;
+
+
+    }
+
+    public Integer getAllFlightCustomerAsList() {
+
+
+        ObjectOutputStream oos = null;
+        FileOutputStream fos = null;
+        List<Integer> numberOfFlightCustomers = null;
+
+
+        try {
+
+
+            fos = new FileOutputStream(Config.PERSIST_STORAGE_OUTPUT_LOCATION);
+            oos = new ObjectOutputStream(fos);
+
+            EntityManager em = emf.createEntityManager();
+
+
+            String queryString = new String("FROM Customer");
+
+
+            javax.persistence.Query query = em.createQuery(queryString);
+
+            Session session = em.unwrap(Session.class);
+
+            /*
+            Hibernate OGM: not implemented yet
+             */
+
+            ScrollableResults results = session
+                    .createQuery("FROM FlightCustomer")
+                    .setCacheMode(CacheMode.IGNORE)
+                    .scroll(ScrollMode.FORWARD_ONLY);
+
+
+            numberOfFlightCustomers = getAllFlightCustomerIDs();
+
+            logger.info("Get all flightcustomer TA begins");
+            tm.setTransactionTimeout(Config.TRANSACTION_TIMEOUT);
+            tm.begin();
+
+
+            long queryStart = System.currentTimeMillis();
+
+
+            while (results.next()) {
+                FlightCustomer fc = (FlightCustomer) results.get(0);
+                fc.toString();
+            }
+            results.close();
+
+
+            long queryEnd = System.currentTimeMillis();
+
+
+            oos.writeObject(null);
+            fos.flush();
+            fos.close();
+            oos.flush();
+            oos.close();
+
+            em.flush();
+            em.close();
+            tm.commit();
+
+            logger.info("Get all flightcustomer TA ends");
+
+            long queryTime = queryEnd - queryStart;
+
+            logger.info("Found " + numberOfFlightCustomers.size() + " flightcustomers in " + queryTime + " ms.");
+
+            String writeToFile = new String(Config.PERSISTENCE_UNIT_NAME + " READ  : " + numberOfFlightCustomers.size() + " " + queryTime + "\n");
+            Files.write(Paths.get(Config.LOG_STORAGE_LOCATION), writeToFile.getBytes(), CREATE, APPEND);
+
+
+        } catch (NotSupportedException e) {
+            e.printStackTrace();
+        } catch (SystemException e) {
+            e.printStackTrace();
+        } catch (HeuristicMixedException e) {
+            e.printStackTrace();
+        } catch (HeuristicRollbackException e) {
+            e.printStackTrace();
+        } catch (RollbackException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return numberOfFlightCustomers.size();
+
+
+    }
+
+
 }
+
