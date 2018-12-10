fb16c5645b07c18fa7c70c2c8f34c57f1407f45c
sbtarit <1514520@stud.hs-mannheim.de>
2018-12-10 10:21:42 +0100

    add PopulatorController

---
+++ src/main/java/de/hsma/jens/controllers/PopulatorController.java
@@ -0,0 +1,69 @@
+package de.hsma.jens.controllers;
+
+import de.hsma.jens.models.FlightCustomer;
+import de.hsma.jens.tools.Config;
+import org.apache.log4j.Logger;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.transaction.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static java.nio.file.StandardOpenOption.APPEND;
+import static java.nio.file.StandardOpenOption.CREATE;
+
+public class PopulatorController {
+    private static Logger logger = Logger.getRootLogger();
+    //accessing JBoss's Transaction can be done differently but this one works nicely
+    TransactionManager tm = com.arjuna.ats.jta.TransactionManager.transactionManager();
+    //build the EntityManagerFactory as you would build in in Hibernate ORM
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory(Config.PERSISTENCE_UNIT_NAME);
+
+    public void createFlightCustomer(FlightCustomer flightCustomer) {
+        try {
+            logger.info("Create customer TA begins");
+            EntityManager em = emf.createEntityManager();
+            tm.setTransactionTimeout(Config.TRANSACTION_TIMEOUT);
+            tm.begin();
+
+            long queryStart = System.currentTimeMillis();
+
+         //   em.persist(flightCustomer.getFlightCustomerAsList());
+         //   em.persist(flightCustomer);
+
+
+            long queryEnd = System.currentTimeMillis();
+
+            em.flush();
+            em.close();
+            tm.commit();
+
+
+            logger.info("Create customers TA ends");
+            long queryTime = queryEnd - queryStart;
+
+            logger.info("Customers persisted in DB in " + queryTime + " ms.");
+
+            String writeToFile = new String(Config.PERSISTENCE_UNIT_NAME + " CREATE: " + 1 + " " + queryTime + "\n");
+
+            Files.write(Paths.get(Config.LOG_STORAGE_LOCATION), writeToFile.getBytes(), CREATE, APPEND);
+
+
+        } catch (NotSupportedException e) {
+            e.printStackTrace();
+        } catch (SystemException e) {
+            e.printStackTrace();
+        } catch (HeuristicMixedException e) {
+            e.printStackTrace();
+        } catch (HeuristicRollbackException e) {
+            e.printStackTrace();
+        } catch (RollbackException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
